e63b51a190a4bcd30edcd939bc292b684db94bd7
jhaoanyang <jhaoanyang@gmail.com>
2022-06-16 14:34:10 +0800

    chore(global): init

---
+++ craco.config.js
@@ -0,0 +1,9 @@
+const cracoPluginScopedCss = require('craco-plugin-scoped-css');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: cracoPluginScopedCss,
+    },
+  ],
+};
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scoped.scss';
+
+function App() {
+  return (
+    <main />
+  );
+}
+
+export default App;
